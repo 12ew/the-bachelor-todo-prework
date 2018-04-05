efa0132208bdf9f799878206c9c183d828f3307d
github username <github email address>
2018-04-05 18:51:01 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,86 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |key, value|
+    value.each do |x, z|
+      # binding.pry
+
+      if x["satus"] == "Winner".capitalize!
+
+        x.each do |k, v|
+          # binding.pry
+          name = x["name"].split.shift
+
+            return "#{name}" if x["status"] ==  "Winner" && key == "#{season}"
+
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |x, z|
+      # binding.pry
+
+      if x["satus"] == "Winner".capitalize!
+
+        x.each do |k, v|
+          # binding.pry
+          name = x["name"]
+
+            return "#{name}" if x["occupation"] ==  "#{occupation}"
+
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  city_count = 0
+  data.each do |key, value|
+    value.each do |x, z|
+
+        x.each do |k, v|
+          if v == "#{hometown}"
+          city_count += 1
+
+        end
+      end
+    end
+  end
+  city_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |key, value|
+    value.each do |x, z|
+      # binding.pry
+
+      # if x["satus"] == "Winner".capitalize!
+
+        x.each do |k, v|
+          # binding.pry
+          occupation = x["occupation"]
+
+            return "#{occupation}" if x["hometown"] ==  "#{hometown}"
+
+        # end
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  count = 0
+
+  data[season].each do |hash|
+      age += (hash["age"]).to_i
+      count += 1
+    end
+    (age/count.to_f).round(0)
 end
